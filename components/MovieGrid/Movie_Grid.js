753408bde7dbaf855619d2929cbb152d0b449ecb
Daniel Jowett <dbjowett@gmail.com>
2022-01-22 05:28:14 +0900

    Deploy

--- components/MovieGrid/Movie_Grid.js
+++ components/MovieGrid/Movie_Grid.js
@@ -1,5 +1,4 @@
-// import MovieCard from '../MovieCard/Movie_Card';
-import MovieCard2 from '../../components/MovieCard/Movie_Card';
+import MovieCard from '../MovieCard/Movie_Card';
 import classes from '../MovieGrid/movie_grid.module.css';
 
 export default function MovieGrid({ title, movies }) {
@@ -8,7 +7,7 @@ export default function MovieGrid({ title, movies }) {
       <div className={classes.heading}>{title}</div>
       <ul className={classes.grid}>
         {movies.map((movie) => (
-          <MovieCard2 key={movie.id} movie={movie} />
+          <MovieCard key={movie.id} movie={movie} />
         ))}
       </ul>
     </div>
