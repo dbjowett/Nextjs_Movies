094cb89f3aa9307b166344b7a6496ff50bb1a1f8
Daniel Jowett <dbjowett@gmail.com>
2022-01-25 20:38:30 +0900

    Fix sidebar auto-close on mobile, fix background colour of hamburger button, fix sizing on login form, and more

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 ## See [Live](https://nextjs-movies-theta.vercel.app/) ⚡⚡
 
 ## Features
+
 - Next.js [Static Generation](https://nextjs.org/docs/basic-features/pages#static-generation-recommended)
 - Next.js [SSR](https://nextjs.org/docs/basic-features/pages#server-side-rendering)
-- Next.js [useSWR (front-end data fetching)](https://swr.vercel.app) 
+- Next.js [useSWR (front-end data fetching)](https://swr.vercel.app)
 - CSS Modules
 - Node (Express)
 - Axios [Axios](https://axios-http.com)
--- components/Sidebar/Sidebar.js
+++ components/Sidebar/Sidebar.js
@@ -1,5 +1,5 @@
 import Link from 'next/dist/client/link';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { FaFireAlt, FaCalendarAlt, FaStar, FaSearch, FaSignInAlt, FaBars } from 'react-icons/fa';
 import SidebarLogo from '../Layout/SidebarLogo';
 import classes from './sidebar.module.css';
@@ -7,42 +7,58 @@ import classes from './sidebar.module.css';
 export default function Sidebar() {
   const [isOpen, setIsOpen] = useState(true);
 
-  function clickHandler() {
-    setIsOpen((prev) => !prev);
-    console.log(isOpen);
+  const clickHandler = () => setIsOpen((prev) => !prev);
+
+  function linkClick() {
+    const size = window.innerWidth;
+    if (size < 1000) {
+      setIsOpen(false);
+    }
   }
+
+  useEffect(() => setIsOpen(false), []);
 
   return (
     <div>
       <button className={classes.hamburger} onClick={clickHandler}>
         <FaBars size={35} />
       </button>
-      <nav className={`${classes.sidebar} ${!isOpen ? classes.open : ''}`}>
+      <nav className={`${classes.sidebar} ${isOpen ? classes.open : ''}`}>
         <SidebarLogo />
         <div className={classes.sidebarContainer}>
           <div className={classes.sidebarLink}>
             <FaFireAlt />
-            <Link href='/popular'>Popular</Link>
+            <Link href='/popular'>
+              <a onClick={linkClick}>Popular</a>
+            </Link>
           </div>
           <div className={classes.sidebarLink}>
             <FaCalendarAlt />
-            <Link href='/coming_soon'>Coming Soon</Link>
+            <Link href='/coming_soon'>
+              <a onClick={linkClick}>Coming Soon</a>
+            </Link>
           </div>
           <div className={classes.sidebarLink}>
             <FaStar />
-            <Link href='/trending'>Trending</Link>
+            <Link href='/trending'>
+              <a onClick={linkClick}>Trending</a>
+            </Link>
           </div>
           <div className={classes.sidebarLink}>
             <FaSearch />
-            <Link href='/search'>Search</Link>
+            <Link href='/search'>
+              <a onClick={linkClick}>Search</a>
+            </Link>
           </div>
         </div>
         <div className={classes.btnContainer}>
           <Link href='/auth'>
-            <button className={classes.button}>
-              Login
-              <FaSignInAlt size={15} />
-            </button>
+            <a onClick={linkClick}>
+              <button className={classes.button}>
+                Login
+                <FaSignInAlt size={15} />
+              </button>
+            </a>
           </Link>
         </div>
       </nav>
